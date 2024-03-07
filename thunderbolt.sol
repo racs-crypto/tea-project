b68965f240b3563705671e906f5fdedb939699c5
racs-crypto <128711321+racs-crypto@users.noreply.github.com>
2024-03-07 18:31:45 +0530

    thunderbolt.sol

---
+++ thunderbolt.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact marvelboy@dmail.ai
+contract Thunderbolt is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    constructor(address initialOwner)
+        ERC20("Thunderbolt", "TDB")
+        ERC20Permit("Thunderbolt")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
